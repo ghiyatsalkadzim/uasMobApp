03a14ec30d53c02b863ce0286fbe9aaa33fac15a
ghiyatsalkadzim <ghiyatsakk@gmail.com>
2023-01-10 12:19:11 +0700

    update 2

--- uas_template_des_22-main/app/src/main/java/edu/upi/cs/yudiwbs/uas_template/ViewModelFragmentSatu.java
+++ uas_template_des_22-main/app/src/main/java/edu/upi/cs/yudiwbs/uas_template/ViewModelFragmentSatu.java
@@ -1,8 +1,22 @@
 package edu.upi.cs.yudiwbs.uas_template;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.SavedStateHandle;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class ViewModelFragmentSatu extends ViewModel {
-    public MutableLiveData<String> ip;
-}
+      /*  private SavedStateHandle savedStateHandle;
+        public LiveData<List<String>> filteredData;
+        public UserViewModelJava(SavedStateHandle savedStateHandle) {
+            this.savedStateHandle = savedStateHandle;
+            LiveData<String> queryLiveData = savedStateHandle.getLiveData("query");
+            filteredData = Transformations.switchMap(queryLiveData, query -> {
+                return repository.getFilteredData(query);
+            });
+        }*/
+
+    }
